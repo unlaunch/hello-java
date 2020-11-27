1f42c489a2a64914e411da1b2b5eb38555325221
Umer Mansoor <umermk3@gmail.com>
2020-11-27 13:12:19 -0800

    [minor improvements] README, log4j2 properties, improved error messaging and pom.

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # hello-java
 
-The Unlaunch Hello-Java is a demo to describe how to use Unlaunch Java Sdk in your application. It is a maven project.
-For more details you can visit Unlaunch documentation https://docs.unlaunch.io/docs
+> Hello Unlaunch, from Java!
+
+Unlaunch is a free feature flag service. Please visit [https://www.unlaunch.io/](https://www.unlaunch.io/) to sign up
+ for a new account today!
+ 
+ This is a **demo** project to describe how to use [Unlaunch Java SDK](https://docs.unlaunch.io/docs/sdks/java-sdk) in your application. It is a maven project.
+For more details, read our [Getting Started](https://docs.unlaunch.io/docs/getting-started) tutorial.
 
 # Build Procedure
+1. Download code. Edit Hello.java class set your Unlaunch SDK_KEY and FEATURE_FLAG_KEY you want to evaluate as: 
 
-1. Download code. Edit Hello.java class set your unluanch SDK_KEY and FEATURE_FLAG_KEY that you want to evaluate as 
-	
-	  static final String SDK_KEY = "your-sdk-key";
-
-  	static final String FEATURE_FLAG_KEY = "your-flag-key";
+```
+static final String SDK_KEY = "your-sdk-key";
+static final String FEATURE_FLAG_KEY = "your-flag-key";
+```
 
 On command line run as:
-	
-	  mvn clean compile assembly:single
 
-	  java -jar target/hello-java-1.0-SNAPSHOT.jar 
+```
+mvn clean compile assembly:single
+java -jar target/hello-java-1.0-SNAPSHOT.jar 
+```
 
-The Hello example will print "Variation served is {variation}"
+The Hello example will print: **Variation served is {variation}** with the evaluated variation.
--- src/main/java/io/unlaunch/sdk/Hello.java
+++ src/main/java/io/unlaunch/sdk/Hello.java
@@ -1,7 +1,3 @@
-/*
- * This is a demo HelloWorld app to describe how to use Unlaunch Java Sdk.
- * 
- */
 package io.unlaunch.sdk;
 
 import io.unlaunch.UnlaunchClient;
@@ -10,19 +6,30 @@ import java.util.concurrent.TimeoutException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * This is a demo app to describe how to use Unlaunch Java SDK.
+ * @author fahmina
+ */
 public class Hello {
 
     private static final Logger LOG = LogManager.getLogger(Hello.class);
 
-    // Set SDK_KEY to your Unlaunch SDK key 
+    // EDIT ME! Set SDK_KEY to your Unlaunch SDK key.
     private static final String SDK_KEY = "";
 
-    // Set FEATURE_FLAG_KEY to your feature flag key to evaluate
+    // EDIT ME!  Set FEATURE_FLAG_KEY to your feature flag key to evaluate
     private static final String FEATURE_FLAG_KEY = "";
 
     public static void main(String[] args) {
-        if (SDK_KEY.equals("")) {
-            LOG.error("Please edit Hello.java to set SDK_KEY to Unlaunch SDK key.");
+        if (SDK_KEY == null || SDK_KEY.isEmpty()) {
+            LOG.error("You must edit Hello.java to set SDK_KEY to Unlaunch SDK key. \n" +
+                    "Please visit https://docs.unlaunch.io/docs/sdks/sdk-keys for more information.");
+            System.exit(1);
+        }
+
+        if (FEATURE_FLAG_KEY == null || FEATURE_FLAG_KEY.isEmpty()) {
+            LOG.error("You must edit Hello.java to set FEATURE_FLAG_KEY to key of the flag you wish to fetch. \n" +
+                    "Please visit https://docs.unlaunch.io/docs/getting-started for more information.");
             System.exit(1);
         }
 
@@ -40,6 +47,10 @@ public class Hello {
         String variation = client.getVariation(FEATURE_FLAG_KEY, "user-id-123");
         
         LOG.info("Variation served is {}", variation);
+
+        if (variation.equals("control")) {
+            LOG.info("'control' variation indicates that Unlaunch Client didn't connect with the server.");
+        }
         
         // shutdown the client to flush any events or metrics 
         client.shutdown();
